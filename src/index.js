f6bd830ca8961e7214764771fff69ead3520360f
root <+>
2023-10-21 08:12:59 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,69 @@
-let addToy = false;
+// index.js
+document.addEventListener('DOMContentLoaded', () => {
+  const toyCollection = document.getElementById('toy-collection');
+  const toyForm = document.getElementById('toy-form');
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
+
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(toy => {
+        const card = createToyCard(toy);
+        toyCollection.appendChild(card);
+      });
+    });
+
+    toyForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const name = document.getElementById('toy-name').value;
+    const image = document.getElementById('toy-image').value;
+    const likes = 0;
+    const newToy = { name, image, likes };
+
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+      .then(response => response.json())
+      .then(data => {
+        const card = createToyCard(data);
+        toyCollection.appendChild(card);
+      });
+
+    toyForm.reset();
   });
+
+
+
+  function createToyCard(toy) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = toy.name;
+
+    const img = document.createElement('img');
+    img.src = toy.image;
+    img.classList.add('toy-avatar');
+
+    const p = document.createElement('p');
+    p.textContent = `${toy.likes} Likes`;
+
+    const button = document.createElement('button');
+    button.classList.add('like-btn');
+    button.id = toy.id;
+    button.textContent = 'Like ❤️';
+
+    card.appendChild(h2);
+    card.appendChild(img);
+    card.appendChild(p);
+    card.appendChild(button);
+
+    return card;
+  }
 });
+
